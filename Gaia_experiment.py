ed364e75d0d94856a99ed0bd4423b40c13eeb641
Kaze Wong <kazewong.physics@gmail.com>
2022-06-28 17:25:26 -0400

    Add Gaia experiement. Don't add and build tree with jax array, turbo slow

--- Gaia_experiment.py
+++ Gaia_experiment.py
@@ -5,26 +5,55 @@ import jax.numpy as jnp
 import jax
 from annoy import AnnoyIndex
 import numpy as np
+import h5py
+from sklearn.preprocessing import QuantileTransformer
 
-f = 5  # Length of item vector that will be indexed
+batch_size = 1000
+n_epoch = 100
+
+data = h5py.File("/mnt/home/apricewhelan/projects/gaia-scratch/data/gaiadr3-apogee-bprp-Xy.hdf5","r")
+
+X_ = data['X'][:]
+Y_ = data['y'][:]
+clean_index = np.where(np.invert(np.isnan(Y_[:,2])))[0]
+X_ = X_[clean_index]
+Y_ = Y_[clean_index]
+
+# x_scaler = StandardScaler()
+# y_scaler = StandardScaler()
+x_scaler = QuantileTransformer()
+Y_scaler = QuantileTransformer()
+x_scaler.fit(X_)
+Y_scaler.fit(Y_)
+
+x_scale = x_scaler.transform(X_)
+y_scale = Y_scaler.transform(Y_)
+
+x_index = x_scale[:int(len(x_scale)*0.6)]
+x_train = x_scale[int(len(x_scale)*0.6):int(len(x_scale)*0.8)]
+y_train = y_scale[int(len(y_scale)*0.6):int(len(y_scale)*0.8)]
+x_test = x_scale[int(len(x_scale)*0.8):]
+y_test = y_scale[int(len(y_scale)*0.8):]
+
+f = 23  # Length of item vector that will be indexed
 
 t = AnnoyIndex(f, 'angular')
 
-data = np.random.uniform(size=(1000,5))
-for i in range(1000):
-    t.add_item(i, data[i])
-
+for i in range(len(x_index)):
+    if i % 1000 == 0:
+        print(i)
+    t.add_item(i, x_index[i])
 
 t.build(10) # 10 trees
+t.save('test.ann')
 
 key = jax.random.PRNGKey(1071)
-data = jax.random.uniform(key, (1000, 5))
 
-index = []
-for i in range(1000):
-    index.append(t.get_nns_by_item(i, 9)[1:])
+train_index = []
+for i in range(len(x_train)):
+    train_index.append(t.get_nns_by_vector(x_train[i], 9)[1:])
 
-index = jnp.stack(np.array(index))
+train_index = jnp.stack(np.array(train_index))
 
 class AnnoyMLP(nn.Module):
 
@@ -46,23 +75,12 @@ class AnnoyMLP(nn.Module):
     def __call__(self, index):
         x = self.embbeding(index)
         x = x.reshape(-1, self.embeddingQuerySize*self.embeddingFeatures)
-        print(x.shape)
         for layer in self.layers[:-1]:
             x = self.activation(layer(x))
         return self.layers[-1](x)
 
 
-model = AnnoyMLP(100, 8, 2, [64, 64, 1])
+model = AnnoyMLP(x_index.shape[0], 8, 2, [64, 64, 1])
 params = model.init(key, jnp.zeros((1,8), dtype=int))
 
-def loss(index):
-    pred = model.apply(params, index)
-    return pred
 
-def loss_query(x_batched):
-    index = []
-    for i in range(len(x_batched)):
-        index.append(t.get_nns_by_item(i, 9)[1:])
-    index = jnp.stack(np.array(index))
-    pred = model.apply(params, index)
-    return pred
